174d767dd5f04603ebcf2f5ef9a93bc8add181d1
Muhammad Fajrul Khaq <fajrulm05@gmail.com>
2025-05-11 00:11:55 +0700

    fix styling viewResume

--- src/pages/view/ViewResume.jsx
+++ src/pages/view/ViewResume.jsx
@@ -3,6 +3,7 @@ import ResumePreview from "@/components/custom/ResumePreview";
 import { Button } from "@/components/ui/button";
 import { ResumeInfoContext } from "@/context/ResumeInfoContext";
 import GlobalApi from "@/service/GlobalApi";
+import { Download } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -27,18 +28,23 @@ function ViewResume() {
     <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
       <div id="no-print">
         <Header />
-        <div className="my-10 mx-10 md:mx-20 lg:mx-36">
-          <h2 className="text-center text-2xl font-medium">Congrats! Your Ultimate AI geneares Resume is ready!</h2>
-          <p className="text-center text-gray-400">Now you are ready to download your resume and you can share unique resume url with your friends and family</p>
+        <div className="w-full flex justify-center">
+          <div className="mt-10 mx-10 md:mx-20 lg:mx-36">
+            <h2 className="text-center text-2xl font-medium">Congrats! Your Ultimate AI geneares Resume is ready!</h2>
+            <p className="text-center text-gray-400">Now you are ready to download your resume and you can share unique resume url with your friends and family</p>
+            <div className="flex justify-end mt-5">
+              <Button onClick={handleDownload}>
+                <Download /> Download
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="my-10 mx-10 md:mx-20 lg:mx-150">
-        <div className="flex justify-end gap-5 my-5">
-          <Button onClick={handleDownload}>Download</Button>
-          <Button>Share</Button>
-        </div>
-        <div id="print-area">
-          <ResumePreview />
+      <div className="w-full flex justify-center">
+        <div className="my-10 mx-10 md:mx-20 lg:mx-36 w-[800px]">
+          <div id="print-area">
+            <ResumePreview />
+          </div>
         </div>
       </div>
     </ResumeInfoContext.Provider>
